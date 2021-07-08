8336390c5cc95ab0ae3bd99d8f940c1828fd1b96
zarif-al <zarif_al96@outlook.com>
2021-07-08 19:10:33 +0600

    testing graphics card issue

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ export async function getStaticProps(context) {
 
   const graphics_data = await db
     .collection("Items")
-    .find({ category: "Graphics%20Card" })
+    .find({ category: encodeURIComponent("Graphics Card") })
     .project({
       _id: 0,
       productCode: 1,
